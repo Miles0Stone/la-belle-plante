7e6c0125a257b87796a3a027143c14c94557491f
Your Name <you@example.com>
2021-12-15 16:35:22 +0100

    not found code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageAccueilComponent } from './pages/page-accueil/page-accueil.component';
 import { PageDetailsComponent } from './pages/page-details/page-details.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: 'home',component: PageAccueilComponent},
-  {path: 'details',component: PageDetailsComponent}
+  {path: 'details',component: PageDetailsComponent},
+  {path: 'page-not-found',component: PageNotFoundComponent},
+  {path: '**', redirectTo: 'page-not-found' }
+
 ];
 
 @NgModule({
